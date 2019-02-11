1db88e0f3f5bef5b149e9c47b02c032a6ac17fce
OkkoManninen <47212819+OkkoManninen@users.noreply.github.com>
2019-02-11 13:36:27 +0200

    Delete Google.py

--- Google.py
+++
@@ -1,8 +0,0 @@
-from google.cloud import vision
-from google.cloud.vision import types
-
-client = vision.ImageAnnotatorClient()
-image = vision.types.Image()
-image.source.image_uri = 'link'
-resp = client.text_detection(image=image)
-print('\n'.join([d.description for d in resp.text_annotations]))
