6177edfcc357c8341caa1feb4cd3ca484465d3ad
OkkoManninen <47212819+OkkoManninen@users.noreply.github.com>
2019-02-11 13:32:56 +0200

    Create google_API.py

---
+++ google_API.py
@@ -0,0 +1,24 @@
+link = "Link_here"
+
+gcloud services enable vision.googleapis.com
+export GOOGLE_CLOUD_PROJECT="<PROJECT_ID>"
+gcloud iam service-accounts create my-vision-sa \
+  --display-name "my vision service account"
+  gcloud iam service-accounts keys create ~/key.json \
+  --iam-account my-vision-sa@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com
+  export GOOGLE_APPLICATION_CREDENTIALS="/home/${USER}/key.json"
+  
+  
+  ipython
+  
+  
+  
+  
+  from google.cloud import vision
+from google.cloud.vision import types
+
+client = vision.ImageAnnotatorClient()
+image = vision.types.Image()
+image.source.image_uri = 'link'
+resp = client.text_detection(image=image)
+print('\n'.join([d.description for d in resp.text_annotations]))
